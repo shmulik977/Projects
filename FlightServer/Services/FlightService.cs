08e690a0d6157d3ba2310b14f7dd663aba00da8f
shmulik.levi977 <shmulik.levi977@gmail.com>
2022-08-16 20:57:09 +0300

    Revert "Revert "Update""
    This reverts commit 640b4f11ca7d3736d161372539a9d81fa4dff28d.

--- FlightServer/Services/FlightService.cs
+++ FlightServer/Services/FlightService.cs
@@ -1,5 +1,4 @@
-﻿using FlightServer.DAL.Repositories;
-using FlightServer.Infra;
+﻿using FlightServer.Infra;
 using Shared.Lib.Infra;
 using Shared.Lib.Models;
 using System.Collections.Generic;
@@ -11,16 +10,16 @@ namespace FlightServer.Services
     public class FlightService : IFlightService
     {
         private readonly IFlightRepository _flightReposiory;
-        Timer timer;
-        LinkedList<IPlanned> WaitingList;
-        private IPlanned planned;
+        Timer _timer;
+        LinkedList<IPlanned> _waitingList;
+        private IPlanned _planned;
 
-        public FlightService(IFlightRepository flightReposiory,IMockRepository mockRepository=null)
+        public FlightService(IFlightRepository flightReposiory)
         {
             _flightReposiory = flightReposiory;
-            timer = new Timer(5000);
-            timer.Elapsed += Timer_Elapsed;
-            WaitingList = new LinkedList<IPlanned>();
+            _timer = new Timer(5000);
+            _timer.Elapsed += Timer_Elapsed;
+            _waitingList = new LinkedList<IPlanned>();
         }
 
         public async Task<IList<FlightHistory>> GetAllFlights()
@@ -38,48 +37,47 @@ namespace FlightServer.Services
             return await _flightReposiory.GetAllPlannedLandings();
         }
 
+        public async Task<List<StatusStation>> GetStations()
+        {
+            return await _flightReposiory.GetStations();
+        }
 
         public async Task CreatePlannedFlight()
         {
-            Flight flight = await _flightReposiory.CreateFlight();
-            PlannedFlights plannedFlights = new PlannedFlights() { FlightId = flight.Id, SourceStationId = 10, DestinationStationId = "6,7" };
-            bool IsEndStation = _flightReposiory.CreatePlannedFlight(plannedFlights, out planned);
-            WaitingList.AddLast(planned);
-            if (!IsEndStation)
-                Wait(IsEndStation);
+            var flight = await _flightReposiory.CreateFlight();
+            var plannedFlights = new PlannedFlights() { FlightId = flight.Id, SourceStationId = 10, DestinationStationId = "6,7" };
+            bool isEndStation = _flightReposiory.CreatePlannedFlight(plannedFlights, out _planned);
+            _waitingList.AddLast(_planned);
+            if (!isEndStation)
+                Wait(isEndStation);
         }
 
         public async Task CreatePlannedLandings()
         {
-            Flight flight = await _flightReposiory.CreateFlight();
-            PlannedLanding plannedLanding = new PlannedLanding() { FlightId = flight.Id, SourceStationId = 9, DestinationStationId = "1" };
-            bool IsEndStation = _flightReposiory.CreatePlannedLanding(plannedLanding, out planned);
-            Wait(IsEndStation);
+            var flight = await _flightReposiory.CreateFlight();
+            var plannedLanding = new PlannedLanding() { FlightId = flight.Id, SourceStationId = 9, DestinationStationId = "1" };
+            bool isEndStation = _flightReposiory.CreatePlannedLanding(plannedLanding, out _planned);
+            Wait(isEndStation);
         }
 
         private void Wait(bool IsEndStation)
         {
-            WaitingList.AddLast(planned);
+            _waitingList.AddLast(_planned);
             if (!IsEndStation)
-                timer.Start();
+                _timer.Start();
         }
 
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            timer.Stop();
-            LinkedListNode<IPlanned> x = WaitingList.First;
-            if (WaitingList.Count > 0)
+            _timer.Stop();
+            var flights = _waitingList.First;
+            if (_waitingList.Count > 0)
             {
-                planned = WaitingList.First.Value;
-                WaitingList.RemoveFirst();
-                bool IsEndStation = _flightReposiory.CheckIfStationEmpty(x.Value, out planned);
+                _planned = _waitingList.First.Value;
+                _waitingList.RemoveFirst();
+                bool IsEndStation = _flightReposiory.CheckIfStationEmpty(flights.Value, out _planned);
                 Wait(IsEndStation);
             }
-        }
-
-        public async Task<List<StatusStation>> GetStations()
-        {
-            return await _flightReposiory.GetStations();
         }
     }
 }
